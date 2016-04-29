70bdcbaeccb4f2708a1fda0f8d1efec5f9ed90cd
Isabella Defilippis Villarroel <isastedef@yahoo.com>
2016-04-28 23:48:25 -0400

    reproductor e intentos de mejora

---
+++ Spotifoe/buscador.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Motor de busqueda
+	</title>
+	<link rel = "stylesheet" href = "formato.css">
+	<meta http-equiv="Content-Type" contents="text/html; charset=UTF-8">
+</head>
+<body 
+	class = "fondo";>
+
+	<h2
+		class = "centerTitulos">
+		BUSCA LA CANCION DE TU PREFERENCIA
+	</h2>
+	
+	<div>
+		<img src = "images/lupa.jpg" id = "ima">
+	</div>
+	<div>
+		<form action = "buscador.php" method = "post" name = "form1" class = "center"> 
+			
+			<input type = "text" name = "buscador" placeholder = "Ingrese nombre de la cancion o album a buscar" style="width:284px;height:30px">
+			<br>
+			<br>
+			<input type = "submit" value = "Buscar" name = "buscar">
+		</form>
+	</div>
+	<div class = "resultados">
+	<?php
+		if( isset($_POST['buscar']) )
+		{
+			$host = "localhost";
+			$dbname = "musica";
+			$username = "root";
+			$PASSWORD = "";
+
+
+			$Nombre = $_POST["buscador"];
+			$Album = $_POST["buscador"];
+
+
+			//Seleccionar el servidor:
+			$connection = mysqli_connect($host, $username, $PASSWORD, $dbname) or die ("No se pudo conectar con el servidor de la BD.");
+			//echo "Conexion establecida a MySQL<br />";
+
+			//Seleccionar la BD con la que vamos a trabajar:
+			//mysql_select_db($dbname, $connection) or die ("La BD no existe.");
+			
+			//echo "BD selecconada: " . " " . $dbname. "<br />";
+
+			//para cancion
+			$consulta = "SELECT * FROM CANCION WHERE Nombre = '$Nombre'";
+
+			//PARA EJECUTAR LA CONSULTA:
+			$ejecutar_consulta = mysqli_query($connection, $consulta) or die ("No se pudo ejecutar la consulta en la BD.");
+			//echo "Resultados De Busqueda: <br />";
+			//echo "    <br />";
+
+			//mostrar el resultado de la consulta, dentro de un ciclo y en una variable ingresamos la funcion mySQL fetch array (la ejecicion dela consulta la guarda en un arreglo)
+			?>
+			
+			<ul>
+			
+			<?php
+			while ($registro = mysqli_fetch_array($ejecutar_consulta))
+			{
+				echo "<li><a href='interfaz_reproductor.php?codCanc=".$registro["Cod_cancion"]."'>".$registro["Nombre"]."</a></li>";
+				//echo "Album: ". $registro["Cod_album"]. "   ". $registro["Fecha_lan"]. "<br />";
+			}
+			?>
+			</ul>
+			<?php
+			
+			
+			//para album
+			//echo "<h4> BUSCANDO...</h4>";
+			$consulta = "SELECT * FROM ALBUM WHERE Nombre = '$Album'";
+
+			//PARA EJECUTAR LA CONSULTA:
+			$ejecutar_consulta = mysqli_query($connection, $consulta) or die ("No se pudo ejecutar la consulta en la BD.");
+			//echo "Resultados De Busqueda: <br />";
+			//echo "    <br />";
+
+			//mostrar el resultado de la consulta, dentro de un ciclo y en una variable ingresamos la funcion mySQL fetch array (la ejecicion dela consulta la guarda en un arreglo)
+			?>
+			
+			<ul>
+			
+			<?php
+			while ($registro = mysqli_fetch_array($ejecutar_consulta))
+			{
+				echo "<li><a href='http://www.w3schools.com/html/'>".$registro["Cod_album"]." - ".$registro["Nombre"]."</a></li>";
+				//echo "Año: ". $registro["Ano"]. "   ". $registro["Fecha_lan"]. "<br />";
+			}
+			?>
+			</ul>
+			<?php
+		}
+	?>
+	<div>
+</body>
+</html>
---
+++ Spotifoe/datos_cancion.php
@@ -0,0 +1,33 @@
+<html>
+<header>
+Datos de la cancion
+</header>
+<body>
+<font color = "white">
+<?php
+include ("connection.php");
+$codCancion = $_GET['codCanc'];
+$conn = connect();
+$queryAlbum = "SELECT album.nombre from cancion,album where album.Cod_album = (select Cod_album from cancion where Cod_cancion = ".$codCancion.")";
+$resulAlbum = mysqli_query($conn, $queryAlbum);
+$arrayAlbum = mysqli_fetch_array($resulAlbum);
+$queryNombre = "SELECT nombre from cancion where Cod_cancion = ".$codCancion;
+$resulNombre = mysqli_query($conn, $queryNombre);
+$arrayNombre = mysqli_fetch_array($resulNombre);
+?>
+<div align = "center "> <img src="images/music.jpg" alt="MUSICA" style="width:304px;height:228px;"> </div>
+<div align = "center"> <h1> <?php echo $songName = $arrayNombre["nombre"]?> </h1> 
+					   <h2>
+						<?php
+							$queryArtista = "SELECT artista.nombre FROM artista, artistacancion WHERE artista.Cod_artista = artistacancion.Cod_artista and artistacancion.Cod_cancion = ".$codCancion;
+							$resulArtista = mysqli_query($conn, $queryArtista);
+							while ($arrayArista = mysqli_fetch_array($resulArtista)) {
+								echo $arrayArista["nombre"]." ";
+							}
+						?>
+					   </h2> 
+					   <h3><?php echo $albumName = $arrayAlbum["nombre"]?></h3> 
+</div>
+<div align = "center">
+</body>
+</html>
