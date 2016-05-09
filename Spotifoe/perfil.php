a131d1beec882e9bc73c2e53ffa15cd32cb79b6f
Sergio Oporto <Sergio.E.Oporto@ieee.org>
2016-05-09 18:59:53 -0400

    editado el formato del perfil

--- Spotifoe/perfil.php
+++ Spotifoe/perfil.php
@@ -9,7 +9,7 @@
 		$username = $res['USERNAME'];
 		$email = $res['EMAIL'];
 		$fecha = $res['FECHA'];
-		if ($res['GENERO']=1){
+		if ($res['GENERO']==1){
 			$genero = "Masculino";
 		}
 		else {
@@ -31,15 +31,19 @@
 	<div id="container">
 		<?php include("menuIzquierda.php"); ?>
 		<div id="content">
-			<span><b>UserID: </b><?php echo $id;?></span><br>
-			<span><b>Nombre: </b><?php echo $nombre;?></span><br>
-			<span><b>Usuario: </b><?php echo $username;?></span><br>
-			<span><b>Email: </b><?php echo $email;?></span><br>
-			<span><b>Fecha: </b><?php echo $fecha;?></span><br>
-			<span><b>Género: </b><?php echo $genero;?></span><br>
-			<a href="modificarUsuario.php?userID="<?php echo $id;?>>
-				<input type="button" class="button" value="ACTUALIZAR DATOS">
-			</a>
+			<div id="datos">
+				<span><b>UserID: </b><?php echo $id;?></span><br>
+				<span><b>Nombre: </b><?php echo $nombre;?></span><br>
+				<span><b>Usuario: </b><?php echo $username;?></span><br>
+				<span><b>Email: </b><?php echo $email;?></span><br>
+				<span><b>Fecha: </b><?php echo $fecha;?></span><br>
+				<span><b>Género: </b><?php echo $genero;?></span><br>
+			</div>
+			<div id="divBoton">
+				<a href="modificarUsuario.php?userID=<?php echo $id;?>">
+					<input type="button" class="button" value="EDITAR PERFIL">
+				</a>
+			</div>
 		</div>
 	</div>
 </body>
