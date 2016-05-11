9399b0b857bda75d99413d0b1a29143ad33b5e80
Isabella Defilippis Villarroel <isastedef@yahoo.com>
2016-05-11 01:10:12 -0400

    se mejoro top25

--- Spotifoe/top25Escuchadas.php
+++ Spotifoe/top25Escuchadas.php
@@ -1,18 +1,20 @@
-<?php 
-	$codUsuario = $_GET['userID'];
-?>
 <!DOCTYPE html>
 <header>
 	<link rel = "stylesheet" href = "style/formatoLista.css">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat|Inconsolata">
 </header>
 <body class = "fondo">
+<?php 
+	$codUsuario = $_GET['userID'];
+?>
+<div id="container">
 
-<div style="margin-left:0px;margin-top:0px;">
 	<?php include("menuIzquierda.php"); ?>
-</div>
 
 <div>
-	<h2 class = "centerTitulos">Top 25 - Lo mejor solo para ti</h2>
+	<div id="header">
+		<p id="titulo">Top 25 - Lo mejor solo para ti</p>
+	</div>
 <?php
 	include ("consultasSQL.php");
 	$resulLista = top25Escuchadas();
@@ -21,11 +23,12 @@
 	<ol>
 <?php
 	while ($arrayLista = mysqli_fetch_array($resulLista)) {
-		echo "<li class = 'LiItem'><a class = 'LiItemA' href = 'opciones.php?codCanc=".$arrayLista['Cod_cancion']."&userID=".$codUsuario."'>".$arrayLista['Nombre']."</a>".$arrayLista['Veces_escuch']."</li>";
+		echo "<li class = 'LiItem'><a class = 'LiItemA' href = 'opciones.php?codCanc=".$arrayLista['Cod_cancion']."&userID=".$codUsuario."'> <h3>".$arrayLista['Nombre']." </h3> </a>".$arrayLista['Veces_escuch']." veces reproducida"."</li>";
 	}
 ?>
 	</ol>
 	<div>
+</div>
 </div>
 </body>
 </html>
